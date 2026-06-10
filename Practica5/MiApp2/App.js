ed1da62d145526ef8269ddf7dc569afa12c6ce51
Ozitx <123046490@upq.edu.mx>
2026-06-10 10:35:38 -0600

    Práctica No.6 Est. y comp. 2

--- Practica5/MiApp2/App.js
+++ Practica5/MiApp2/App.js
@@ -3,20 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {Saludo} from './components/Saludo';
 import {Saludo2} from './components/Saludo2';
+import { Perfil } from './components/Perfil';
 
 /* Zona 2: Main - Hogar de los componentes */
 export default function App() {
   return (
     <View style={styles.container}>
+      
+      <Text>---------- Mi Perfil :3 ----------</Text>
+      <Perfil></Perfil>
 
-      <Text>---------Componentes nativos-----------</Text>
+  {/* <Text>---------Componentes nativos-----------</Text>
       <Image source={require('./assets/wave.png')}/>
       <Text> Hola mundo RN!</Text>
       <Text>-------------Componente-------------</Text>
       <Saludo/>
       <Text>-----------Compomente compuesto-----------</Text>
       <Saludo2></Saludo2>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
+
     </View>
   );
 }
---
+++ Practica5/MiApp2/components/Perfil.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export const Perfil = () => {
+    return (
+        <View>
+            <Text> Nombre: Cynthia Reséndiz Ramos</Text>
+            <Text> Carrera: Ingeniería en sistemas computacionales</Text>
+            <Text> Materia: Programación Móvil</Text>
+            <Text> Cuatrimestre: Noveno</Text>
+        </View>
+    )
+}
