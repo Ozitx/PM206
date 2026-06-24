0d001dad475db707d2dee7787ac9e502fc10d85d
Ozitx <123046490@upq.edu.mx>
2026-06-23 19:08:48 -0600

    Practica No.11: Pressable & Switch

--- Practica5/MiApp2/screens/PressableScreen.js
+++ Practica5/MiApp2/screens/PressableScreen.js
@@ -1,15 +1,56 @@
 /* Zona 1: Importaciones de componentes y archivos */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Switch } from 'react-native';
+import { useState } from 'react';
 
 /* Zona 2: Main - Hogar de los componentes */
 export default function PressableScreen() {
+  const [buttonText, setButtonText] = useState("Dame Click");
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <View style={styles.container}>
+    <View style = {[styles.container, {backgroundColor: isDarkMode ? "#000000": "#ffffff"}]}>
 
-        <Text> Aqui va la practica de  Migue </Text>     
+      <StatusBar style={isDarkMode ? "light": "dark"}/>
 
-      <StatusBar style="auto" /> 
+      <Pressable
+        style = {styles.button}
+        onPress = {() => {
+          console.log("se presionó el botón");
+          setButtonText("Boton Presionado");
+        }}
+        
+        onPressIn={() => {
+          console.log("Se acaba de preisonar el boton");
+        }}
+
+        onPressOut={() => {
+          console.log("Se acaba de soltar el boton");
+        }}
+
+        onLongPress={() =>{
+          console.log("Presion prolongada");
+          setButtonText("Presion prolongada detectada");
+        }}
+        >
+    
+      <Text style = {StyleSheet.buttonText}>{buttonText}</Text>
+
+      </Pressable>
+
+      <Text style={[styles.text, {color: isDarkMode ? "#ffffff": "#000000"}]}>
+        Dark Mode
+      </Text>
+
+      <Switch
+        
+        value={isDarkMode}
+        onValueChange={(previousState) => setIsDarkMode((previousState) => 
+          !previousState
+        )}
+        trackColor={{false: "#767577", true: "lightblue"}}
+        thumbColor={"#f4f3f4"}       
+      />
 
     </View>
   );
@@ -17,11 +58,32 @@ export default function PressableScreen() {
 
 /* Zona 3: Estilos y posicionamiento */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection:'column'
-  },
+    container: {
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 50 
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "white",
+        textAlign: "center"
+    },
+    switchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "80%", 
+        paddingHorizontal: 10
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold"
+    }
 });
